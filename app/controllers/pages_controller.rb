13c4439a1871b9c1c460094fab563c0935d2ba1c
PawelShi <schis-pm@yandex.ru>
2014-07-24 22:29:45 +0600

    Converted Old maket on Middleman to ROR4

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,23 @@
 class PagesController < ApplicationController
   def home
+  	@current_page = {title: '', comment: ''}
   end
 
   def tools
+  	@current_page = {title: 'Услуги', 
+        comment: ''#Описание всех возможных видов работ, что мы выполняем'
+      }
   end
 
   def projects
+  	@current_page = {title: 'Проекты', 
+        comment: ''#Список доступных для просмотра проектов.'
+      }
   end
 
   def contacts
+  	@current_page = {title: 'Контакты', 
+        comment: ''#'Содержит информацию для связи.'
+      }
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+	def image_box(img_src, text, url)
+		str = '<div class="thumbnail noborder nopadding">' +
+            image_tag(img_src, width: "100%" ) +
+            '<div class="caption">
+              <h5 class="text-center">' + text + '</h5>
+            </div>
+          </div>'
+        
+        link_to(raw(str),url)
+	end
 end
