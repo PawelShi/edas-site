fa02f3c018526b8a3e78b3ada4d6adc2ff8161fd
PawelShi <schis-pm@yandex.ru>
2014-08-21 16:56:49 +0600

    Change model Service

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140821104802) do
+ActiveRecord::Schema.define(version: 20140821105621) do
 
   create_table "microposts", force: true do |t|
     t.date     "start_dt"
